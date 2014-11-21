da41800a3899bf5c63e968c0232c9c6d5002e756
Jonas Thiel <jt@neopoly.de>
2014-11-21 12:18:30 +0100

    Add AssemblyInfo

---
+++ Assets/NeoNetwork/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NeoNetwork")]
+[assembly: AssemblyDescription("A class library for very simple network operations in Unity3D")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Neopoly GmbH")]
+[assembly: AssemblyProduct("NeoNetwork")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("faa64a34-9f01-465f-add3-9e5765347c8b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
