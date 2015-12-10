7c01ceffb68664eacd8de5e7af0c23bf1552af9e
Niclas Mietz <github@mietz.io>
2015-12-10 10:15:34 +0100

    Replace PseudoEnum through a real enum

--- Assets/NeoNetwork/Http/HttpMethod.cs
+++ Assets/NeoNetwork/Http/HttpMethod.cs
@@ -1,30 +1,9 @@
 namespace Neo.Network.Http {
   /// <summary>
-  /// Pseudo-Enum class for describing HTTP statis codes
+  /// Describes the possible HttpMethod that are currently supported
   /// </summary>
-  public sealed class HttpMethod {
-    /// <summary>
-    /// A GET request
-    /// </summary>
-    public static readonly HttpMethod GET  = new HttpMethod("GET");
-    /// <summary>
-    /// A POST request
-    /// </summary>
-    public static readonly HttpMethod POST = new HttpMethod("POST");
-    //more if needed
-
-    private readonly string name;
-
-    private HttpMethod(string name){
-      this.name = name;
-    }
-
-    /// <summary>
-    /// Return the name
-    /// </summary>
-    /// <returns>the name</returns>
-    public override string ToString(){
-      return name;
-    }
+  public enum HttpMethod {
+    POST,
+    GET
   }
 }
